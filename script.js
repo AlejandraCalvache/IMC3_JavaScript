dea19158f22019ee1dda14720f5fadee126f625a
AlejandraCalvache <alejandracalvache8@hotmail.com>
2024-12-01 22:25:31 -0500

    docs: :art: test devops

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.getElementById('imcForm').addEventListener('submit', function(event) {
     // Clasificación según el IMC
     let clasificacion = '';
     if (imc < 18.5) {
-        clasificacion = 'Bajo peso';
+        clasificacion = 'Peso Bajo';
     } else if (imc >= 18.5 && imc < 24.9) {
         clasificacion = 'Peso normal';
     } else if (imc >= 25 && imc < 29.9) {
